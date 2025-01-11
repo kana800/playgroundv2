d579aef4a403d22184cc932fa4029fab70c75d04
kana800 <64830745+kana800@users.noreply.github.com>
2025-01-11 21:09:00 +0530

    euler problem 9; added a solution from the thread

--- euler/9.c
+++ euler/9.c
@@ -1,6 +1,7 @@
 // https://projecteuler.net/problem=9
 #include <stdio.h>
 	
+
 int main(int argc, char **argv)
 {
 	for ( int a = 1; a <= 1000; a++)
@@ -19,6 +20,17 @@ int main(int argc, char **argv)
 			}
 		}
 	}
+
+	// from pier https://projecteuler.net/thread=9#284
+	//a= 2mn; b= m^2 -n^2; c= m^2 + n^2;
+	//a + b + c = 1000;
+	//2mn + (m^2 -n^2) + (m^2 + n^2) = 1000;
+	//2mn + 2m^2 = 1000;
+	//2m(m+n) = 1000;
+	//m(m+n) = 500;
+	//m>n;
+	//m= 20; n= 5;
+	//a= 200; b= 375; c= 425;
 
 	return 0;
 }
